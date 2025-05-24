8345cb5312d57855352009fbfe6beb3498bc1106
HexedRevii <143884194+hexedrevii@users.noreply.github.com>
2025-05-24 19:44:59 +0300

    Add -n to show user&host.

--- los/main.cpp
+++ los/main.cpp
@@ -1,17 +1,22 @@
 #include "kernel.h"
 #include "release.h"
 #include "user.h"
+#include <cstring>
 #include <optional>
 #include <print>
 #include <string>
 #include <string_view>
-int main()
+int main(int argc, char** argv)
 {
-  std::optional<std::string_view>  uname = los::get_user();
-  std::optional<const std::string> hname = los::get_hostname();
+  if (argc > 1 && std::strcmp(argv[1], "-n") == 0)
+  {
+    std::optional<std::string_view>  uname = los::get_user();
+    std::optional<const std::string> hname = los::get_hostname();
 
-  if (uname && hname) std::println("{}@{}", *uname, *hname);
-  std::println("\n{}", los::get_release());
+    if (uname && hname) std::println("{}@{}\n", *uname, *hname);
+  }
+
+  std::println("{}", los::get_release());
 
   if (std::optional<const std::string> kernel = los::kernel())
     std::println("{}", *kernel);
